1c3f1a29218cf0c0c783e84964aae630dc700c27
Roman Vashchenko <roman1991dp@gmail.com>
2024-03-27 00:53:26 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,15 +2,40 @@ import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
 
-const form = document.querySelector('.feedback-form');
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  inputEL: document.querySelector('input[type="email"]'),
+  textareaEL: document.querySelector('textarea[name="message"]'),
+};
 
-console.log(form);
+refs.form.addEventListener('input', throttle(onInputForm, 500));
 
-form.addEventListener('input', throttle(onInputForm, 500));
+refs.form.addEventListener('submit', onSubmitForm);
 
 const objData = {};
+
+const alertMessage = 'ДАНИХ НЕМАЄ';
+
+getDataLocalStorage();
 
 function onInputForm(e) {
   objData[e.target.name] = e.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(objData));
+}
+
+function getDataLocalStorage() {
+  if (localStorage.getItem(STORAGE_KEY)) {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    const parseData = JSON.parse(savedData);
+    refs.inputEL.value = parseData.email ? parseData.email : '';
+    refs.textareaEL.value = parseData.message ? parseData.message : '';
+    return parseData;
+  }
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  console.log(getDataLocalStorage());
+  localStorage.removeItem(STORAGE_KEY);
+  refs.form.reset();
 }
